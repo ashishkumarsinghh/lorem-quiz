9e4c1a939eac57b08d5e8f5b3dcee498fa30b14c
ashish kumar singh <ashishkumarsingh@outlook.com>
2018-09-26 13:04:56 +0900

    mongoose and mlab setup with env vars

--- server/index.js
+++ server/index.js
@@ -1,11 +1,28 @@
 const hapi = require("hapi")
+const mongoose = require("mongoose")
+const dotenv = require("dotenv")
 
 const server = hapi.server({
     port: 4000, 
     host: 'localhost'
 })
 
+const initVars = () => {
+    dotenv.config();
+}
+
+const connectDB = () => {
+    mongoose.connect("mongodb://"+process.env.DBUSER+":"+
+    `${encodeURIComponent(process.env.DBPASS)}`+"@"+process.env.DBHOST, { useNewUrlParser: true });
+    mongoose.connection.once("open", () => {
+        console.log("connected to database.");
+    })
+};
+
 const init = async () => {
+
+    initVars();
+    connectDB();
     server.route({
         method: "GET",
         path: '/',
